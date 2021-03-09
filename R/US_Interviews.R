050a3b00992a041672c8d85d07adb4120826bf78
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-03-09 15:30:04 +0100

    Added US_Interviews dataset and updated script

--- R/US_Interviews.R
+++ R/US_Interviews.R
@@ -8,32 +8,27 @@ library(usethis)
 
 get_text <- function(source_link) {
   interview_page = read_html(source_link)
-  interview_text = interview_page %>% html_nodes("#block-system-main p") %>% html_text() %>% paste(collapse = ",")
+  interview_text = interview_page %>% html_nodes(".field-docs-content p") %>% html_text() %>% paste(collapse = ",")
   return(interview_text)
 }
 
-get_participants <- function(source_link) {
-  interview_page = read_html(source_link)
-  interview_participants = interview_page %>% html_nodes("i") %>% html_text() %>% paste(collapse = ",")
-  return(interview_participants)
-}
+US_Interviews = data.frame()
 
 for (page_result in seq(from = 0, to = 95, by = 1)) {
   link = paste0("https://www.presidency.ucsb.edu/documents/app-categories/presidential/interviews?page=",
                 page_result)
   page = read_html(link)
 
-  title <- page %>% html_nodes(".field-title p") %>% html_text()
+  title <- page %>% html_nodes(".field-title a") %>% html_text()
   date <-  page %>% html_nodes(".date-display-single") %>% html_text()
   president <- page %>% html_nodes(".margin-top a") %>% html_text()
-  source_links <- page %>% html_nodes(".field-title p") %>% html_attr("href") %>% paste("https://www.presidency.ucsb.edu", . , sep = "")
+  source_links <- page %>% html_nodes(".field-title a") %>% html_attr("href") %>% paste0("https://www.presidency.ucsb.edu", . , sep = "")
   text <- sapply(source_links, FUN = get_text, USE.NAMES = FALSE)
-  participants <- sapply(source_links, FUN = get_participants, USE.NAMES = FALSE)
 
-  US_interviews = rbind(US_interviews, data.frame(title, date, participants, moderator, source_links, text, stringsAsFactors = FALSE))
+  US_Interviews = rbind(US_interviews, data.frame(title, date, president, source_links, text, stringsAsFactors = FALSE))
 
   print(paste("Page:", page_result))
 
 }
 
-usethis::use_data(US_interviews)
+usethis::use_data(US_Interviews)
