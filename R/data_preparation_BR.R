152e31450ff848099652c618ac60927c4690bff0
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-06-21 22:51:10 +0200

    Created a data_preparation_BR script for Brazil data

---
+++ R/data_preparation_BR.R
@@ -0,0 +1,20 @@
+# Data Preparation Brazil
+# Henrique Sposito
+
+# For Brazil, at the time being, Cesar 2020 dataset on oral remarks is being used.
+# The data is scraped from the presidential library and covers all official presidential speeches
+# and remarks from January 1985 to July 2020.
+
+load("~/GitHub/Poldis/data/BR_oral.rda")
+summary(BR_oral)
+# Should I start from 1990 here because of direct elections?
+
+# Please cite:
+
+# Debates
+
+# Campaign Remarks
+
+# Interviews
+
+# For more details about collection, please see the Codebook.
