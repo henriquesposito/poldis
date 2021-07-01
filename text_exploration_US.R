5eeedd71ca3448c94f60ef05fbbb2b9a6b821d73
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-01 19:00:59 +0200

    Started exploring campaign texts more closely and develop exploration script

--- text_exploration_US.R
+++ text_exploration_US.R
@@ -10,8 +10,7 @@ library(tm)
 library(wordcloud)
 library(wordcloud2)
 library(mgsub)
-library(pbapply)
-library(skmeans)
+library(pbapply)library(skmeans)
 library(tidytext)
 library(clue)
 library(cluster)
@@ -38,11 +37,9 @@ cleanCorpus<-function(corpus, customStopwords){
   return(corpus)
 }
 
-# First, let's load datasets for US
+### Let's start with one dataset at a time
+
 ucamp <- US_campaign
-udeb <- US_debates
-uora <- US_oral
-uint <- US_interviews
 
 # Get variable names standardized and drop source
 ucamp <- ucamp %>% rename(doc_id = title) %>%
@@ -54,17 +51,130 @@ camp_corpus <- VCorpus(DataframeSource(ucamp))
 stops <- stopwords('english')
 camp_corpus <- cleanCorpus(camp_corpus, stops)
 
-# Repeat the same operations for all datasets
+# Let's explore word frequencies of the datasets as a whole, but first we need to get term document matrixes for all datasets
+camp_DTM  <- DocumentTermMatrix(camp_corpus)
+camp_DTMm <- as.matrix(camp_DTM)
+
+# Get most frequent terms
+camp_WFM <- data.frame(term = names(colSums(camp_DTMm)),
+                   freq = colSums(camp_DTMm))
+camp_WFM <- camp_WFM[order(camp_WFM$freq, decreasing = T),]
+rownames(camp_WFM) <- NULL
+head(camp_WFM, 30)
+# Very generic 30 most frequent terms...
+
+# Instead, let's look at bigrams and trigrams
+# Let's also start to use tidytext as sizes become too large for base
+# get text only
+tcamp <- VCorpus(VectorSource(ucamp$text))
+tcamp <- cleanCorpus(tcamp, stops)
+tcamp <- tidy(tcamp)
+tcampt <- tcamp %>%
+  unnest_tokens(bigram, text , token = "ngrams", n = 3, n_min = 2) %>%
+  dplyr::count(bigram, sort = TRUE) %>%
+  ungroup()
+tcampt
+# A bit more informative...
+# Let's plot the 50 most common bigrams
+pal <- brewer.pal(8,"Dark2")
+tcampt %>% with(wordcloud(bigram, n, random.order = FALSE, max.words = 50, colors=pal))
+# Some make sures, tax cuts, new jobs and more. Do you think this differes for the other settings?
+
+# For the sake of it, let's take a look at sentiment for all texts toegther, that is, are texts for debates
+# mostly positive or negatives?
+camp_DTMt <- tidy(camp_DTM)
+camp_sent <- camp_DTMt %>% select(term, count)
+camp_sent <- aggregate(count ~ term, data = camp_sent, FUN=sum)
+camp_sent <- camp_sent %>% arrange(desc(count))
+camp_sentscr <- camp_sent %>%
+  inner_join(get_sentiments("bing"), by = c(term = "word")) %>%
+  arrange(desc(count))
+# Okay, but let's try to quantify this
+csent_scr_pos <- camp_sentscr %>% select(count, sentiment) %>% filter(sentiment == "positive")
+csent_scr_neg <- camp_sentscr %>% select(count, sentiment) %>% filter(sentiment == "negative")
+sum(csent_scr_pos$count)/sum(csent_scr_neg$count) # Mostly positive overall by almost a factor of two
+
+# How about if we try and see which sentiments were overall present
+camp_sentnrc <- camp_sent %>%
+  inner_join(get_sentiments("nrc"), by = c(term = "word")) %>%
+  arrange(desc(count))
+camp_sentnrc # note some words get multiple sentiments
+# Let's remove positive and negative
+camp_sentnrc <- camp_sentnrc %>% filter(sentiment != "positive") %>% filter(sentiment != "negative")
+camp_sentnrc <- aggregate(count ~ sentiment, data = camp_sentnrc, FUN=sum)
+camp_sentnrc # Trust appears the most followed by anticipatons, joy and fear
+# Let's plot this
+chartJSRadar(scores = camp_sentnrc,
+             labs = camp_sentnrc$sentiment,
+             labelSize = 20, showLegend = FALSE, main = "Sentiment in All Campaign Texts")
+
+# Since we are interested in comparisons, what if we broadly compare two speakers as Trump and Obama for example
+obama_camp <- ucamp %>% filter(speaker == "Barack Obama")
+summary(obama_camp)
+trump_camp <- ucamp %>% filter(speaker == "Donald J. Trump")
+summary(trump_camp)
+# Many more obs for Obama...
+# Let's clean them
+obama_corpus <- VCorpus(DataframeSource(obama_camp))
+obama_corpus <- cleanCorpus(obama_corpus, stops)
+obama_DTM  <- DocumentTermMatrix(obama_corpus)
+obama_DTMm <- as.matrix(obama_DTM)
+trump_corpus <- VCorpus(DataframeSource(trump_camp))
+trump_corpus <- cleanCorpus(trump_corpus, stops)
+trump_DTM  <- DocumentTermMatrix(trump_corpus)
+trump_DTMm <- as.matrix(trump_DTM)
+# Let's checkout word frequencies for both
+obama_WFM <- data.frame(term = names(colSums(obama_DTMm)),
+                       freq = colSums(obama_DTMm))
+obama_WFM <- obama_WFM[order(obama_WFM$freq, decreasing = T),]
+rownames(obama_WFM) <- NULL
+head(obama_WFM, 30)
+trump_WFM <- data.frame(term = names(colSums(trump_DTMm)),
+                        freq = colSums(trump_DTMm))
+trump_WFM <- trump_WFM[order(trump_WFM$freq, decreasing = T),]
+rownames(trump_WFM) <- NULL
+head(trump_WFM, 30)
+# Not very infomative...
+# Bigrams for each might help
+t_obamac <- tidy(obama_corpus)
+t_obamact <- t_obamac %>%
+  unnest_tokens(bigram, text , token = "ngrams", n = 3, n_min = 2) %>%
+  dplyr::count(bigram, sort = TRUE) %>%
+  ungroup()
+t_trumpc <- tidy(trump_corpus)
+t_trumpct <- t_trumpc %>%
+  unnest_tokens(bigram, text , token = "ngrams", n = 3, n_min = 2) %>%
+  dplyr::count(bigram, sort = TRUE) %>%
+  ungroup()
+# Let's compare
+t_obamact %>% with(wordcloud(bigram, n, random.order = FALSE, max.words = 50, colors=pal)) # midle class, tax cuts, health care
+t_trumpct %>% with(wordcloud(bigram, n, random.order = FALSE, max.words = 50, colors=pal)) # Hillary clinton, make america, america great
+# Shall we get sentiment? Let's do it
+
+
+
+### Debates
+
+udeb <- US_debates
+
 udeb <- udeb %>% rename(doc_id = Title, text = Text, speaker = Speakers, date = Date) %>%
   arrange(doc_id, text, speaker, date)
 deb_corpus <- VCorpus(DataframeSource(udeb))
 deb_corpus <- cleanCorpus(deb_corpus, stops)
 
-uora <- uora %>% rename(doc_id = title) %>%
-  select(-source_links) %>%
-  arrange(doc_id, text, speaker, date)
-ora_corpus <- VCorpus(DataframeSource(uora))
-ora_corpus <- cleanCorpus(ora_corpus, stops)
+deb_DTM  <- DocumentTermMatrix(deb_corpus)
+deb_DTMm <- as.matrix(deb_DTM)
+
+deb_WFM <- data.frame(term = names(colSums(deb_DTMm)),
+                      freq = colSums(deb_DTMm))
+deb_WFM <- deb_WFM[order(deb_WFM$freq, decreasing = T),]
+rownames(deb_WFM) <- NULL
+head(deb_WFM, 30)
+# Similar generic words...
+
+### Interviews
+
+uint <- US_interviews
 
 uint <- uint %>% rename(doc_id = title) %>%
   select(-source_links) %>%
@@ -72,12 +182,36 @@ uint <- uint %>% rename(doc_id = title) %>%
 int_corpus <- VCorpus(DataframeSource(uint))
 int_corpus <- cleanCorpus(int_corpus, stops)
 
-# Let's explore word frequencies of the datasets as a whole, but first we need to get term document matrixes for all datasets
-camp_DTM  <- DocumentTermMatrix(camp_corpus)
-camp_DTM <- as.matrix(camp_DTM)
+int_DTM  <- DocumentTermMatrix(int_corpus)
+int_DTMm <- as.matrix(int_DTM)
 
-deb_DTM  <- DocumentTermMatrix(deb_corpus)
-deb_DTM <- as.matrix(deb_DTM)
+int_WFM <- data.frame(term = names(colSums(int_DTMm)),
+                      freq = colSums(int_DTMm))
+int_WFM <- int_WFM[order(int_WFM$freq, decreasing = T),]
+rownames(int_WFM) <- NULL
+head(int_WFM, 30)
+# Similar generic words...
+
+### Oral
+
+uora <- US_oral
+
+uora <- uora %>% rename(doc_id = title) %>%
+  select(-source_links) %>%
+  arrange(doc_id, text, speaker, date)
+ora_corpus <- VCorpus(DataframeSource(uora))
+ora_corpus <- cleanCorpus(ora_corpus, stops)
 
 ora_DTM  <- DocumentTermMatrix(ora_corpus)
-ora_DTM <- as.matrix(ora_DTM) # Issue with size here
+ora_DTMt <- tidy(ora_DTM) # Issue with size here hence why we conveted to tidy
+
+ora_WFM <- ora_DTMt %>% select(term, count)
+ora_WFM <- aggregate(count ~ term, data = ora_WFM, FUN=sum)
+ora_WFM <- ora_WFM %>% arrange(desc(count))
+head(ora_WFM, 30)
+# Okay, this has not been all that helpful..
+
+# get sentiment easy with tidy
+ora_sent <- ora_DTMt %>%
+  inner_join(get_sentiments("bing"), by = c(term = "word")) %>%
+  arrange(desc(count))
