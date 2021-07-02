4eead53908dae06e1d3e8d0d3849d3796f0e401e
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-02 18:19:24 +0200

    Continued with the exploration script and explored debates text a bit further

--- text_exploration_US.R
+++ text_exploration_US.R
@@ -1,7 +1,7 @@
 # Text Pre Processing and Exploration US Data
 # Henrique Sposito
 
-# Let's load some packages and helper functions first
+# Let's load some packages
 
 library(dplyr)
 library(ggplot2)
@@ -18,6 +18,7 @@ library(lexicon)
 library(radarchart)
 library(readr)
 
+# Some helper functions and options
 tryTolower <- function(x){
   y = NA
   try_error = tryCatch(tolower(x), error = function(e) e)
@@ -37,8 +38,11 @@ cleanCorpus<-function(corpus, customStopwords){
   return(corpus)
 }
 
-### Let's start with one dataset at a time
+pal <- brewer.pal(8,"Dark2")
+stops <- stopwords('english')
 
+### Let's start with one dataset at a time
+load("~/GitHub/Poldis/data/US_campaign.rda")
 ucamp <- US_campaign
 
 # Get variable names standardized and drop source
@@ -48,7 +52,6 @@ ucamp <- ucamp %>% rename(doc_id = title) %>%
 # create a corpus from data frame
 camp_corpus <- VCorpus(DataframeSource(ucamp))
 # clean corpus = remove stopwords, punctuations and lower case
-stops <- stopwords('english')
 camp_corpus <- cleanCorpus(camp_corpus, stops)
 
 # Let's explore word frequencies of the datasets as a whole, but first we need to get term document matrixes for all datasets
@@ -76,7 +79,6 @@ tcampt <- tcamp %>%
 tcampt
 # A bit more informative...
 # Let's plot the 50 most common bigrams
-pal <- brewer.pal(8,"Dark2")
 tcampt %>% with(wordcloud(bigram, n, random.order = FALSE, max.words = 50, colors=pal))
 # Some make sures, tax cuts, new jobs and more. Do you think this differes for the other settings?
 
@@ -150,27 +152,109 @@ t_trumpct <- t_trumpc %>%
 t_obamact %>% with(wordcloud(bigram, n, random.order = FALSE, max.words = 50, colors=pal)) # midle class, tax cuts, health care
 t_trumpct %>% with(wordcloud(bigram, n, random.order = FALSE, max.words = 50, colors=pal)) # Hillary clinton, make america, america great
 # Shall we get sentiment? Let's do it
-
-
+obama_sent <- tidy(obama_DTM)
+obama_sent <- obama_sent %>%
+  inner_join(get_sentiments("nrc"), by = c(term = "word")) %>%
+  group_by(sentiment) %>%
+  summarize(obama_sent = sum(count))
+obama_sent$obama_sent <- obama_sent$obama_sent/length(obama_camp$doc_id) # normalize scores for comparison
+trump_sent <- tidy(trump_DTM)
+trump_sent <- trump_sent %>%
+  inner_join(get_sentiments("nrc"), by = c(term = "word")) %>%
+  group_by(sentiment) %>%
+  summarize(trump_sent = sum(count))
+trump_sent$trump_sent <- trump_sent$trump_sent/length(trump_camp$doc_id) # normalize scores for comparisons
+sent_ob_tr <- data.frame(cbind(obama_sent, trump_sent)) %>% select(-sentiment.1)
+sent_ob_tr <- data.frame(sent_ob_tr[,-1], row.names=sent_ob_tr[,1])
+chartJSRadar(scores = sent_ob_tr,
+             labs = rownames(sent_ob_tr),
+             labelSize = 20, showLegend = TRUE, main = "Obama versus Trump Sentiments in Campaign Compared (Normalized by the number of documents for each speaker)")
+# Interesting, they do not seem all that different afterall...
 
 ### Debates
 
+# load data
+load("~/GitHub/Poldis/data/US_debates.rda")
 udeb <- US_debates
-
+# rename
 udeb <- udeb %>% rename(doc_id = Title, text = Text, speaker = Speakers, date = Date) %>%
   arrange(doc_id, text, speaker, date)
 deb_corpus <- VCorpus(DataframeSource(udeb))
 deb_corpus <- cleanCorpus(deb_corpus, stops)
-
+# DTM
 deb_DTM  <- DocumentTermMatrix(deb_corpus)
 deb_DTMm <- as.matrix(deb_DTM)
-
+# Word frequencies
 deb_WFM <- data.frame(term = names(colSums(deb_DTMm)),
                       freq = colSums(deb_DTMm))
 deb_WFM <- deb_WFM[order(deb_WFM$freq, decreasing = T),]
 rownames(deb_WFM) <- NULL
 head(deb_WFM, 30)
 # Similar generic words...
+# Bigrams?
+tdeb <- VCorpus(VectorSource(udeb$text))
+tdeb <- cleanCorpus(tdeb, stops)
+tdeb <- tidy(tdeb)
+tdebt <- tdeb %>%
+  unnest_tokens(bigram, text , token = "ngrams", n = 3, n_min = 2) %>%
+  dplyr::count(bigram, sort = TRUE) %>%
+  ungroup()
+# 50 most common bigrams
+tdebt %>% with(wordcloud(bigram, n, random.order = FALSE, max.words = 50, colors=pal))
+# Yeah, very generic... Is it all that different from most bigrams from campaigns?
+# Since looking at sentiment for all texts may not seem that helpful, how about we compare two presidents?
+# How about we compare George W. Bush and Al Gore (2000 elections)
+# as well as Bill Clinton and George Bush (1992 elections)?
+# Let's select only the year of interest
+deb_comp <- udeb[grep("^1992|^2000", udeb$Date),]
+# Let's remove Ross Perot from sample
+deb_comp <- deb_comp[grep("Ross Perot", deb_comp$Speakers, invert = TRUE),]
+# Agregate these by speakers
+deb_comp <-  aggregate(deb_comp$Text, list(deb_comp$Speakers), paste, collapse =" ")
+# Clean and transfrom into a corpus (I use the SMART dictionary for removing stopwords,
+# more agreesive, here for testing)
+deb_comp <- rename(deb_comp, doc_id = "Group.1", text = "x")
+deb_comp_corpus <- VCorpus(DataframeSource(deb_comp))
+deb_comp_corpus <- cleanCorpus(deb_comp_corpus, stopwords("SMART"))
+deb_comp_DTM <- TermDocumentMatrix(deb_comp_corpus)
+deb_comp_DTMm <- as.matrix(deb_comp_DTM)
+# Let's first look at word frequencies and associations for each speaker
+deb_compt <- tidy(deb_comp_corpus)
+deb_comp_wf <- deb_compt %>%
+  unnest_tokens(word, text) %>%
+  count(id, word, sort = TRUE)
+w_bush <- deb_comp_wf %>% filter(id == " George W. Bush") %>% slice_head(n = 10)
+b_clinton <- deb_comp_wf %>% filter(id == " Bill Clinton") %>% slice_head(n = 10)
+al_gore <- deb_comp_wf %>% filter(id == " Al Gore") %>% slice_head(n = 10)
+bush <- deb_comp_wf %>% filter(id == " George Bush") %>% slice_head(n = 10)
+top_terms <- cbind(w_bush, b_clinton, al_gore, bush)
+# Not super helpful...
+# How about we do sentiment for each speaker?
+w_bush_sent <- deb_comp_wf %>% filter(id == " George W. Bush")
+w_bush_sent <- inner_join(w_bush_sent, get_sentiments("nrc"), by = "word") %>%
+  group_by(sentiment) %>%
+  summarize(w_bush_sent = sum(n))
+b_clinton_sent <- deb_comp_wf %>% filter(id == " Bill Clinton")
+b_clinton_sent <- inner_join(b_clinton_sent, get_sentiments("nrc"), by = "word") %>%
+  group_by(sentiment) %>%
+  summarize(b_clinton_sent = sum(n))
+al_gore_sent <- deb_comp_wf %>% filter(id == " Al Gore")
+al_gore_sent <- inner_join(al_gore_sent, get_sentiments("nrc"), by = "word") %>%
+  group_by(sentiment) %>%
+  summarize(al_gore_sent = sum(n))
+bush_sent <- deb_comp_wf %>% filter(id == " George Bush")
+bush_sent <- inner_join(bush_sent, get_sentiments("nrc"), by = "word") %>%
+  group_by(sentiment) %>%
+  summarize(bush_sent = sum(n))
+sent_deb_sp <- data.frame(cbind(w_bush_sent, b_clinton_sent, al_gore_sent, bush_sent)) %>%
+  select(-c(sentiment.1, sentiment.2, sentiment.3))
+sent_deb_sp <- data.frame(sent_deb_sp[,-1], row.names=sent_deb_sp[,1])
+chartJSRadar(scores = sent_deb_sp,
+             labs = rownames(sent_deb_sp),
+             labelSize = 20, showLegend = TRUE, main = "Sentiments in Debates Compared")
+# Interesting, they do not seem all that different afterall...
+
+
 
 ### Interviews
 
