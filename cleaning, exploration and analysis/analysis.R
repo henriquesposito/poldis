3b892d224e0fc7f49083c4cec792b974408c1d13
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-21 22:58:22 +0200

    Updated analysis script and moved dictionary of terms here

--- cleaning, exploration and analysis/analysis.R
+++ cleaning, exploration and analysis/analysis.R
@@ -7,6 +7,7 @@
 library(dplyr)
 library(readr)
 library(stringr)
+library(stringi)
 
 # Bind all data by speaker and year to create one big dataset for each case
 
@@ -94,4 +95,50 @@ textbr <- purrr::map(BR$text, as.character)
 textbr <- gsub("[[:punct:]]", "", textbr, perl=TRUE)
 BR$text <- textbr
 
-# load dictionaries and get frequencies for speaker-year
+# Let's create a length and dates column for later on
+BR$length <- nchar(BR$text)
+BR$date <- stringr::str_extract(BR$doc_id, "[0-9]{4}")
+US$length <- nchar(US$text)
+US$date <- stringr::str_extract(US$doc_id, "[0-9]{4}")
+
+# Load dictionaries and get frequencies for speaker-year
+# Shall we start with the US?
+# Dictionary of terms for authenticity performances for US
+
+truth_telling <- ("the truth is|this is the truth|not lying|is honest|are honest|honesty|is sincere|are sincere|is true|are true|not a liar|bottom of my heart|I swear|I promise|reassure|I know|is evident”, “are evident”, “I am sure”, “trust me”, “frank”, “up-front”, “come clean”, “coming clean”, “is upfront”, “are upfront|is straightforward|are straightforward")
+
+lie_accusations <- ("not truth|not true|untruthful|is lying|are lying|is a liar|are liars|is dishonest|are dishonest|dishonesty|is fake|are fake|is corrupt|are corrupt|full of lies|not sincere|not honest|is cheating|is a cheater|are cheaters|are tricking|is tricking|be deceived|is deceiving|are deceiving|are a hypocrite|is a hypocrite|is crooked|are crooked|is misleading|has double-standards|are sneaky|is sneaky|two-faced|you are wrong|not correct|fooled by")
+
+consistency <- ("we delivered|check and see|keep my words|kept my word|keep promises|I am responsible|I take responsibility|we are accountable|we are responsible|our duty|my duty|give my word|giving my word|my word|own up my|owning up my|accept responsibility|accept the blame|recognize my mistakes|admit I was wrong")
+
+finger_pointing <- ("inconsistent|irresponsible|their fault|not my fault|they left us with|are responsible|is responsible|costed us|false promises|lack accountability|lacking accountability|not kept|not recognize|he made mistakes|she made mistakes|they made mistakes|not take responsibility|not accountable|blame them|their blame")
+
+origins <- ("I was born|I come from|grew up|my parents|my mom|my mother|my father|my dad|my family|raised|my background|being surrounded by|being exposed to|my siblings|going to school|our local church|Sunday mass|Saturday mass|family tradition|tradition in my house|growing up")
+
+common_sense <- ("is common sense|are common sense|everyone knows|it is undeniable|stating the obvious|everyone agrees|we all know|wisdom|from experience|it is my experience|sound judgment|practical|pragmatic|down-to-earth|realistic|realistically|et me tell you about|is obvious|are obvious")
+
+anti_pc<- ("politically correct|political correctness|PC|plain speaking|speaking my mind|speak my mind|say what I think|saying what I think|going to pretend|not pretend|speak what you think|what you want to hear|butter up|beat around the bush|cut to the chase")
+
+territory <- ("have seen in|have been to|have visited|to be in|came all the way to|back from|was in|we were in|went to|will visit|saw first-hand|to see first-hand|walked around|walked in|people in|will be in|we visited|I visited|travelled to|spend a few days in|spent some time in|I lived|spent time|stayed in")
+
+# Get each performance as frequencies per row
+US$truth <- stringr::str_count(US$text, paste0("(?i)", truth_telling))
+US$lies <- stringr::str_count(US$text, paste0("(?i)", lie_accusations))
+US$consistency <- stringr::str_count(US$text, paste0("(?i)", consistency))
+US$fpoint <- stringr::str_count(US$text, paste0("(?i)", finger_pointing))
+US$origins <- stringr::str_count(US$text, paste0("(?i)", origins))
+US$common_sense <- stringr::str_count(US$text, paste0("(?i)", common_sense))
+US$anti_PC <- stringr::str_count(US$text, paste0("(?i)", anti_pc))
+US$territory <- stringr::str_count(US$text, paste0("(?i)", territory))
+
+# take a quick look at the findings
+US_1985 <- US %>%  filter(date == 1985) %>% select(-text)
+US_1985
+US_1995 <- US %>%  filter(date == 1995) %>% select(-text)
+US_1995
+US_2005 <- US %>%  filter(date == 2005) %>% select(-text)
+US_2005
+US_2015 <- US %>%  filter(date == 2015) %>% select(-text)
+US_2015
+
+truth_time <- US %>% select(truth, date, setting, length) # Get obs per year and plot
