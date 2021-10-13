713ed1ae98fd3c347fa2f65b3208f0fe75a8f9a2
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-10-13 17:21:51 +0200

    Updated context() function to find all matches in text and return a list object. Arguments were also simplified for now.

--- R/context.R
+++ R/context.R
@@ -4,36 +4,43 @@
 #' @param string Character string to be matched.
 #' For multiple strings, please use "|" as a separator.
 #' @param var Text variable
-#' @param context_level At which text level do you want matches to be returned?
+#' @param level At which text level do you want matches to be returned?
 #' Options are sentences, words, and paragraph.
-#' @param context_size How many sentences or words do you want to be returned for matches?
-#' Note that size only works for sentences or words and not paragraphs.
-#' Sizes range from 1 to 3.
-#' @param context_side Which side do want matches?
-#' Default is "both" sides but "right" and "left" are available.
-#' @param ignore.case Should case be ignored?
-#' Default is TRUE.
+#' @importFrom stringr str_detect str_extract_all
+#' @details For sentences, the sentence before and the sentence after match are returned.
+#' For words, 3 words before and 3 words after match are returned.
+#' For paragraph, the whole paragraph of the match is returned.
+#' For paragraph to work properly, please make sure paragraph marks are present in text.
+#' @examples
+#' context(string = "war|weapons of mass destruction|conflict|NATO|peace",
+#' var = US_News_Conferences$text[500], level = "sentences")
 #' @return A list of string matches an their context
 #' @export
-context <- function(string,
-                    var = NULL,
-                    context_level = c("sentences", "words", "paragraph"),
-                    context_size = NULL,
-                    context_side = "both",
-                    ignore.case = TRUE) {
+context <- function(string, var, level = c("sentences", "words", "paragraph")) {
 
-
-  if (is.null(var)) {
-    stop("Please declare the level of the text variable")
+  if (is.null(level)) {
+    stop("Please declare the level of the text to be returned, option are sentences, words or paragraph")
   }
-  if (is.null(context_level)) {
-    stop("Please declare the level of the text to be returned.")
+  if (level == "sentences") {
+    # sentence before and after string match
+    match <- paste0("([^.]+\\.){1}[^.]+(", string, ").*?\\.([^.]+\\.){1}")
+    s <- stringr::str_extract_all(var, match)
   }
-  if (context_level == "sentences" & is.null(context_size) $ ignore.case == TRUE) {
-    # One sentence before and one after string match
-    s <- stringr::str_extract_all(var, paste0(".*?(([^.]+\\.){0,1}[^.]+", string, "*?\\.(.*?\\.){0,1}).*"))
+  if (level == "words") {
+    # three words before and after string match
+    match <- paste0("\\w+ \\w+ \\w+ ", string, " \\w+ \\w+ \\w+")
+    s <- stringr::str_extract_all(var, match)
   }
-
+  if (level == "paragraph") {
+    # whole paragraph match
+    if (stringr::str_detect(var, "\n+\\s+", negate = TRUE))
+    {
+      stop("No paragraph markings were found in text variable, please set level to sentences or words")
+    }
+    paragraph <- strsplit(var, "\n+\\s+")
+    s <- ifelse(stringr::str_detect(string, paragraph), paragraph, "")
+  }
+  s
 }
 
 # # Banco Central em Discursos Presidenciais
