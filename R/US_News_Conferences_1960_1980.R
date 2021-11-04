400366cc1fa2bf5050f3daf23b5c551b7c82ef9d
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-11-04 13:22:26 +0100

    Replaced US News Conferences data sample for a shorter sample with data from 1960 to 1980 only

---
+++ R/US_News_Conferences_1960_1980.R
@@ -0,0 +1,14 @@
+#' US News Conferences Data from 1960 to 1980
+#'
+#' A dataset containing the news conferences from US presidents
+#' from 1960 to 1980.
+#' The dataset was gathered from the American Presidency Project
+#' (https://www.presidency.ucsb.edu/).
+#' @format A data frame with 1210 rows and 5 variables:
+#' \describe{
+#'   \item{president}{US president}
+#'   \item{date}{date of news conference}
+#'   \item{text}{text of news conference}
+#' }
+#' @source \url{https://www.presidency.ucsb.edu/documents/app-categories/presidential/news-conferences}
+"US_News_Conferences_1960_1980"
