78b8b568f56a84e35a94686a83bf6b622bb0cfc0
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2024-02-26 16:15:27 +0100

    Removed non standard files in package

--- R/urgency_analysis.R
+++
@@ -1,7 +0,0 @@
-urgency <- function(text, dictionary, context = "2 sentences",
-                    metadata = NULL, language = "English", keep.all = TRUE) {
-  if (isFALSE(keep.all)) {
-    text <- extract_match(text, dictionary)
-  }
-  text <- extract_context(text, dictionary, context)
-}
