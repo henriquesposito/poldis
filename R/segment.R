04804b71f9e88ce1d7800a3cd336681440b0d424
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2024-03-21 18:42:39 +0100

    Updated arguments in `extract_subjects()` to make the function more flexible

--- R/segment.R
+++ R/segment.R
@@ -1,29 +1,32 @@
-# Functions to help with text segmentation
-
 #' Extract future promises from political discourses
 #'
-#' @param v Text vector.
+#' @param v Text vector or annotated data frame.
 #' @importFrom dplyr filter
 #' @importFrom stringr str_detect
+#' @examples
+#' \donttest{
+#' extract_promises(US_News_Conferences_1960_1980[1:2,3])
+#' }
 #' @export
 extract_promises <- function(v) {
-  tags <- tokens <- sentence <- NULL
+  tags <- tokens <- sentence <- segment_id <- poss <-
+    lemmas <- text <- doc_id <- entities <- sentence_id <- NULL
   if (any(class(v) == "data.frame")) {
     if ("token_id" %in% names(v))
       stop("Please declare a text vector or an annotated data frame at the sentence level.")
   } else v <- suppressMessages(annotate_text(v, level = "sentences"))
-  v <- v |>
-    dplyr::mutate(segment_id = cumsum(grepl("First|Second|Third|Fourth|Fifth|\\?|begin by|start by|begin with|Let",
-                                            v$sentence))) |> # assign segment IDs for merging text that belong together
-    dplyr::select(-c(sentence_id, ntoken)) |>
-    dplyr::group_by(doc_id, segment_id) |>
-    dplyr::reframe(text = stringr::str_c(sentence),
-                     poss = stringr::str_c(poss),
-                     tags = stringr::str_c(tags),
-                     lemmas = stringr::str_c(lemmas),
-                     entities = stringr::str_c(entities))
+  # v <- v |>
+  #   dplyr::mutate(segment_id = cumsum(grepl("First|Second|Third|Fourth|Fifth|\\?|begin by|start by|begin with|Let",
+  #                                           v$sentence))) |> # assign segment IDs for merging text that belong together
+  #   dplyr::select(-c(sentence_id, ntoken)) |>
+  #   dplyr::group_by(doc_id, segment_id) |>
+  #   dplyr::reframe(sentence = stringr::str_c(sentence),
+  #                  poss = stringr::str_c(poss),
+  #                  tags = stringr::str_c(tags),
+  #                  lemmas = stringr::str_c(lemmas),
+  #                  entities = stringr::str_c(entities))
   v <- v |> dplyr::filter(stringr::str_detect(tags, " MD ") |
-                       stringr::str_detect(text, "going to|need to|ready to|is time to|commit to|promise to|intend to|let's"))
+                          stringr::str_detect(sentence, "going to|need to|ready to|is time to|commit to|promise to|intend to|let's"))
   class(v) <- c("promises", class(v))
   v
   # todo: extract related sentences around promises and re-paste together
@@ -31,12 +34,26 @@ extract_promises <- function(v) {
 
 #' Extract most frequent subjects from political discourses
 #'
-#' @param v Text vector.
+#' @param v Text vector or annotated data frame.
 #' @param n Number of subjects
+#' @param method Method to match similar strings.
+#' Defaults to "cosine".
+#' For other methods available please refer to `stringdist::stringdist()`.
+#' @param level Maximum threshold to match strings.
+#' Defaults to 0.1.
+#' Possible values range from 0 to 1.
 #' @import dplyr
 #' @importFrom purrr map_dfr
+#' @importFrom stringr str_squish str_replace_all
+#' @importFrom tm removePunctuation
+#' @importFrom quanteda stopwords
+#' @importFrom stringdist stringdist
+#' @examples
+#' \donttest{
+#' extract_subjects(US_News_Conferences_1960_1980[1:2, 3])
+#' }
 #' @export
-extract_subjects <- function(v, n = 20) {
+extract_subjects <- function(v, n = 20, method = "cosine", level = 0.1) {
   sentence_id <- doc_id <- entity <- group <- subjects <- subject <- NULL
   if (any(class(v) == "data.frame")) {
     if (!"doc_id" %in% names(v)) {
@@ -47,28 +64,28 @@ extract_subjects <- function(v, n = 20) {
     }
   } else v <- suppressMessages(annotate_text(v))
   out <- spacyr::entity_extract(v) |>
-    group_by(doc_id, sentence_id) |>
-    mutate(duplicated = n() > 1,
-           entity = stringr::str_squish(
-             tm::removePunctuation(tm::removeWords(
-             stringr::str_replace_all(tolower(entity), "_", " "),
-             tm::stopwords())))) |>
-    filter(duplicated == FALSE) |>
-    ungroup()
+    dplyr::group_by(doc_id, sentence_id) |>
+    dplyr::mutate(duplicated = n() > 1,
+                  entity = stringr::str_squish(
+                    tm::removePunctuation(tm::removeWords(
+                      stringr::str_replace_all(tolower(entity), "_", " "),
+                      quanteda::stopwords())))) |>
+    dplyr::filter(duplicated == FALSE) |>
+    dplyr::ungroup()
   out <- purrr::map_dfr(out$entity, ~ {
-    i <- which(stringdist::stringdist(., out$entity, "lv") < 2 &
+    i <- which(stringdist::stringdist(., out$entity, method) < level &
                  nchar(out$entity) > 3)
-    tibble(index = i, subjects = out$entity[i])
+    dplyr::tibble(index = i, subjects = out$entity[i])
   }, .id = "group") |>
-    distinct(index, .keep_all = T) |>
-    group_by(group) |>
-    summarize(subject = paste(unique(subjects), collapse = "|"),
-              count = n()) |>
-    arrange(-count) |>
-    ungroup() |>
-    filter(subject != "") |>
-    select(subject) |>
-    slice_head(n = n) |>
+    dplyr::distinct(index, .keep_all = TRUE) |>
+    dplyr::group_by(group) |>
+    dplyr::summarize(subject = paste(unique(subjects), collapse = "|"),
+                     count = n()) |>
+    dplyr::arrange(-count) |>
+    dplyr::ungroup() |>
+    dplyr::filter(subject != "") |>
+    dplyr::select(subject) |>
+    dplyr::slice_head(n = n) |>
     unlist()
   class(out) <- c("subjects", class(out))
   out
@@ -79,14 +96,19 @@ extract_subjects <- function(v, n = 20) {
 
 #' Extract terms related to subjects
 #'
-#' @param v Text vector.
+#' @param v Text vector or annotated data frame.
 #' @param subjects List of subjects.
 #' @param n Number of terms.
 #' @import LSX
 #' @import quanteda
+#' @examples
+#' \donttest{
+#' extract_related_terms(US_News_Conferences_1960_1980[1:2, 3],
+#'                       subjects = c("kennedy", "military"))
+#' }
 #' @export
 extract_related_terms <- function(v, subjects, n = 5) {
-  doc_id <- token <- NULL
+  doc_id <- token <- text <- NULL
   if (any(class(v) == "data.frame")) {
     if (!"doc_id" %in% names(v)) {
       stop("Please declare a text vector or an annotated data frame.")
@@ -95,7 +117,8 @@ extract_related_terms <- function(v, subjects, n = 5) {
       v <- v[["sentence"]]
     } else if ("token_id" %in% names(v)) {
       v <- group_by(doc_id) |>
-        dplyr::summarise(text = paste(token, collapse = " "))
+        dplyr::summarise(text = paste(token, collapse = " ")) |>
+        dplyr::select(text)
     }
   }
   corp <- quanteda::corpus(v) |>
@@ -108,11 +131,15 @@ extract_related_terms <- function(v, subjects, n = 5) {
   out <- list()
   for (i in 1:length(dict)) out[[i]] <- quanteda::dfm_select(dfmt, dict[[i]])
   dfmts <- quanteda::dfm_remove(dfmt, dict) |> cbind(do.call(cbind, out))
-  lss <- LSX::textmodel_lss(dfmts, seeds = dict, k = 300) # model
+  lss <- LSX::textmodel_lss(dfmts, seeds = dict) # model
   terms <- LSX::bootstrap_lss(lss, mode = "terms")[1:n,]
   out <- list()
-  for (i in 1:length(dict)) {
-    out[[names(dict)[i]]] <- c(terms[,c(colnames(terms) %in% dict[[i]])])
+  if (is.null(colnames(terms))) {
+    out[[dict]] <- terms
+  } else {
+    for (i in 1:length(dict)) {
+      out[[names(dict)[i]]] <- c(terms[,c(colnames(terms) %in% dict[[i]])])
+    }
   }
   out <- ifelse(lapply(out, rlang::is_empty), names(out), out)
   names(out) <- names(dict)
@@ -127,9 +154,12 @@ extract_related_terms <- function(v, subjects, n = 5) {
 
 # helper function
 .as_dictionary <- function(a) {
+  out <- list()
+  if (is.null(names(a))) {
+    names(a) <- a
+  }
   names(a) <- ifelse(stringr::str_detect(a, "\\|"),
                      stringr::str_split_i(a, "\\|", i = 1), a)
-  out <- list()
   for (i in names(a)) {
     out[[i]] <- ifelse(stringr::str_detect(a[[i]], "\\|"),
                        stringr::str_split(a[[i]], "\\|"), a[[i]])
@@ -140,26 +170,41 @@ extract_related_terms <- function(v, subjects, n = 5) {
 #' Extract context for string matches
 #'
 #' A function for getting string matches and the context in which they occur.
+#' @param v Text vector or annotated data frame.
 #' @param match Character string to be matched.
 #' For multiple strings, please use "|" as a separator.
-#' @param v Text vector.
 #' @param level At which text level do you want matches to be returned?
+#' Defaults to "sentences".
 #' Options are sentences, words, and paragraph.
 #' @param n Number of sentences or words matched before and after string match.
-#' 1 by default.
+#' Defaults to 1.
 #' That is, one word or one sentence before, and after, string match.
 #' For paragraphs, n is always set to one.
 #' @importFrom stringr str_detect str_extract_all
+#' @importFrom dplyr group_by summarise select
 #' @examples
+#' \donttest{
 #' extract_context(match = "war|weapons of mass destruction|conflict|NATO|peace",
-#' v = US_News_Conferences_1960_1980$text[100],
-#' level = "sentences",
-#' n = 2)
+#'                 v = US_News_Conferences_1960_1980$text[100],
+#'                 level = "sentences", n = 2)
+#' }
 #' @return A list of string matches an their context
 #' @export
-extract_context <- function(match, v, level = c("sentences", "words", "paragraph"), n = 1) {
-  if (is.null(level)) {
-    stop("Please declare the level of the text to be returned, option are sentences, words or paragraph")
+extract_context <- function(match, v, level = "sentences", n = 1) {
+  doc_id <- sentence <- text <- token <- NULL
+  if (any(class(v) == "data.frame")) {
+    if (!"doc_id" %in% names(v)) {
+      stop("Please declare a text vector or an annotated data frame.")
+    }
+    if ("sentence" %in% names(v)) {
+      v <- dplyr::group_by(doc_id) |>
+        dplyr::summarise(text = paste(sentence, collapse = " ")) |>
+        dplyr::select(text)
+    } else if ("token_id" %in% names(v)) {
+      v <- dplyr::group_by(doc_id) |>
+        dplyr::summarise(text = paste(token, collapse = " ")) |>
+        dplyr::select(text)
+    }
   }
   if (level == "sentences") {
     s <- stringr::str_extract_all(v, paste0("([^.]+\\.){0,", n, "}[^.]+(", match, ").*?\\.([^.]+\\.){0,", n, "}"))
@@ -179,18 +224,36 @@ extract_context <- function(match, v, level = c("sentences", "words", "paragraph
 #' Split texts
 #'
 #' Split texts into structured lists of lists according to a split sign.
-#' @param text text variable
+#' @param v Text vector or annotated data frame.
 #' @param splitsign Where do you want to split?
 #' By default sentences (".").
 #' This can also be words, signals or other markers you want.
 #' For special characters, please use escape sign before (i.e. "\\").
 #' @return A splitted list for each row
+#' @importFrom dplyr group_by summarise select
 #' @examples
+#' \donttest{
 #' text <- "This is the first sentence. This is the second sentence."
 #' split_text(text)
+#' }
 #' @export
-split_text <- function(text, splitsign = "\\.") {
-  t <-  strsplit(as.character(text), splitsign)
+split_text <- function(v, splitsign = "\\.") {
+  doc_id <- sentence <- token <- text <- NULL
+  if (any(class(v) == "data.frame")) {
+    if (!"doc_id" %in% names(v)) {
+      stop("Please declare a text vector or an annotated data frame.")
+    }
+    if ("sentence" %in% names(v)) {
+      v <- dplyr::group_by(doc_id) |>
+        dplyr::summarise(text = paste(sentence, collapse = " ")) |>
+        dplyr::select(text)
+    } else if ("token_id" %in% names(v)) {
+      v <- dplyr::group_by(doc_id) |>
+        dplyr::summarise(text = paste(token, collapse = " ")) |>
+        dplyr::select(text)
+    }
+  }
+  t <-  strsplit(as.character(v), splitsign)
   # Add attribute for the number of divisions
   for(i in seq_len(length(t))) {
     attr(t[[i]], "Parts") <- paste0("Number of parts = ", lengths(t[i]))
@@ -201,7 +264,7 @@ split_text <- function(text, splitsign = "\\.") {
 #' Extract text matches
 #'
 #' Get texts in which only certain "matches" occur.
-#' @param text A text variable
+#' @param v Text vector or annotated data frame.
 #' @param match A regex match for a word(s) or expression.
 #' For multiple words, please use "|" to divide them.
 #' @param invert Do you want texts without certain matches to be returned?
@@ -211,23 +274,38 @@ split_text <- function(text, splitsign = "\\.") {
 #' @importFrom purrr map_chr
 #' @return A list of matches of the same length as text variable
 #' @examples
+#' \donttest{
 #' text <- c("This function was created on the 29 September 2021",
 #' "Today is October 12, 2021")
 #' extract_match(text, "October")
+#' }
 #' @export
-extract_match <- function(text, match, invert = FALSE,
+extract_match <- function(v, match, invert = FALSE,
                           ignore.case = TRUE) {
+  doc_id <- token <- text <- NULL
+  if (any(class(v) == "data.frame")) {
+    if (!"doc_id" %in% names(v)) {
+      stop("Please declare a text vector or an annotated data frame.")
+    }
+    if ("sentence" %in% names(v)) {
+      v <- v[["sentence"]]
+    } else if ("token_id" %in% names(v)) {
+      v <- dplyr::group_by(doc_id) |>
+        dplyr::summarise(text = paste(token, collapse = " ")) |>
+        dplyr::select(text)
+    }
+  }
   if (invert == TRUE & ignore.case == FALSE) {
-    t <- lapply(text, function(x) grep(match, x, value = TRUE,
+    t <- lapply(v, function(x) grep(match, x, value = TRUE,
                                        ignore.case = FALSE, invert = TRUE))
   } else if (invert == TRUE & ignore.case == TRUE) {
-    t <- lapply(text, function(x) grep(match, x, value = TRUE,
+    t <- lapply(v, function(x) grep(match, x, value = TRUE,
                                        ignore.case = TRUE, invert = TRUE))
   } else if (invert == FALSE & ignore.case == FALSE) {
-    t <- lapply(text, function(x) grep(match, x, value = TRUE,
+    t <- lapply(v, function(x) grep(match, x, value = TRUE,
                                        ignore.case = FALSE))
   } else {
-    t <- lapply(text, function(x) grep(match, x, value = TRUE,
+    t <- lapply(v, function(x) grep(match, x, value = TRUE,
                                        ignore.case = TRUE))
   }
   t
@@ -235,24 +313,27 @@ extract_match <- function(text, match, invert = FALSE,
 
 #' Extract similarities and differences in texts/segments
 #'
-#' @param v Text vector
+#' @param v Text vector or annotated data frame.
 #' @param comparison Would you like to extract similarities or differences
-#' between treaties?
-#' If not specified, defaults to "similarities".
-#' Alternatively, users can also get the "differences".
+#' between treaties? Options are "similarities" or "differences".
+#' Defaults to "similarities".
 #' @param method A method for checking similarities or differences.
-#' If chosen comparison are similarities, extracts "correlation" between
-#' treaty texts if not specified.
-#' Other similarity methods from `quanteda.textstats::textstat_simil()`
+#' For similarities, defaults to "correlation" method.
+#' Other methods from `quanteda.textstats::textstat_simil()`
 #' include "cosine", "jaccard", "ejaccard", "dice", "edice",
 #' "simple matching", and "hamann".
-#' If chosen comparison are differences, extracts "euclidean" difference
-#' between treaty texts if not specified.
-#' Other difference methods from `quanteda.textstats::textstat_dist()` include
+#' For differences, fedaukts to "euclidean".
+#' Other methods from `quanteda.textstats::textstat_dist()` include
 #' "manhattan", "maximum", "canberra", and "minkowski".
+#' @importFrom quanteda.textstats textstat_simil textstat_dist
+#' @examples
+#' \donttest{
+#' extract_similarities(US_News_Conferences_1960_1980[1:2,3])
+#' }
 #' @export
-extract_similarities <- function(v, comparison, method) {
-  if (missing(comparison) | comparison == "similarities") {
+extract_similarities <- function(v, comparison = "similarities", method) {
+  v <- quanteda::corpus(v)
+  if (comparison == "similarities") {
     if(missing(method)) method = "correlation"
     quanteda.textstats::textstat_simil(quanteda::dfm(v), method = method)
   } else {
