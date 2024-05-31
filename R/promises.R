2b025f3a75b903685d4c9ca3bb039f3d61cbf551
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2024-05-31 19:11:07 +0200

    Renamed and updated promises script

---
+++ R/promises.R
@@ -0,0 +1,40 @@
+#' Extract future promises from political discourses
+#'
+#' @param .data A data frame, promises, or text vector.
+#' For data frames, function will search for "text" variable.
+#' For promises data, function will search for "promises" variable.
+#' @import dplyr
+#' @importFrom stringr str_detect str_remove_all
+#' @examples
+#' \donttest extract_promises(US_News_Conferences_1960_1980[1:2,3])
+#' @export
+extract_promises <- function(v) {
+  tags <- sentence <- lemmas <- sentence_id <- doc_id <- promises <- NULL
+  if (any(inherits(v) == "data.frame")) {
+    if ("token_id" %in% names(v))
+      stop("Please declare a text vector or an annotated data frame at the sentence level.")
+  } else v <- suppressMessages(annotate_text(v, level = "sentences"))
+  v <- v |>
+    dplyr::mutate(lemmas = tolower(lemmas),
+                  promises = ifelse(stringr::str_detect(tags, "PRP MD ")|
+                                      stringr::str_detect(lemmas,
+                                                          "going to|need to|ready to|
+                                     |is time to|commit to|promise to|have to|
+                                     |plan to|intend to|let 's|require|want to"),
+                                    paste(sentence), NA), # detect promises
+                  promises = ifelse(stringr::str_detect(promises, " not |
+                                                        |yesterday|last week|
+                                                        |last month|last year|
+                                                        |prime minister|president|
+                                                        |thank|honor|honour|
+                                                        |applause|greet|laugh|
+                                                        |privilege to|great to|
+                                                        |good to") |
+                                      stringr::str_detect(tags, "MD VB( RB)? VBN|
+                                                         |VBD( RB)? VBN|VBZ( RB)? VBN|
+                                                         |VBD( RB)? JJ|PRP( RB)? VBD TO"),
+                                    NA, promises)) |>
+    dplyr::distinct()
+  class(v) <- c("promises", class(v))
+  v
+}
