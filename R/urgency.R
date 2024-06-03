7180fc72ee7171d4e2556ba5005d40c3e46c2c7e
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2024-06-03 15:08:56 +0200

    Updated `get_urgency()` to work with new dictionary and be more flexible

--- R/urgency.R
+++ R/urgency.R
@@ -4,9 +4,8 @@
 #' For data frames, function will search for "text" variable.
 #' For promises data, function will search for "promises" variable.
 #' @param normalization Would you like urgency scores to be normalized?
-#' By default, urgency scores are normalized by number of words in urgency
-#' dimension and the number of words in text observation.
-#' Other options include "tokens" for the number of words in text observation.
+#' By default, urgency scores are normalized by "tokens",
+#' the number of words in text observation.
 #' Users can also declare "none", for no normalization.
 #' @return A scored data frame.
 #' @import dplyr
@@ -15,30 +14,27 @@
 #' get_urgency(US_News_Conferences_1960_1980[1:10,3])
 #' }
 #' @export
-get_urgency <- function(.data, dictionary, normalize) {
+get_urgency <- function(.data, dictionary, normalize = "tokens") {
+  promises <- frequency <- timing <- degree <- commit <- urgency <- NULL
   # get text variable
   if (inherits(.data, "data.frame")) {
-    text <- .data["text"]
+    text <- .clean_token(.data["text"])
   } else if (inherits(.data, "promises")) {
-    text <- data["promises"]
-  } else text <- .data
+    text <- .clean_token(.data["promises"])
+  } else text <- .clean_token(.data)
   # assign urgency dimensions
-  out <- text
-  out$frequency <- .assign_frequencies(promises)
-  out$timing <- .assign_time(promises)
-  out$degree <- .assign_degree(promises)
-  out$commit <- .assign_commitment(promises)
-  if (missing(normalize)) {
+  out <- data.frame("text" = text)
+  out$frequency <- .assign_frequencies(out[["text"]])/41
+  out$timing <- .assign_timing(out[["text"]])/31
+  out$intensity <- .assign_intensity(out[["text"]])/102
+  out$commitment <- .assign_commitment(out[["text"]])/66
+  if (normalize == "tokens") {
     out <- out |>
-      dplyr::mutate(urgency = (frequency + timing + degree + commit)/nchar(text)) |>
+      dplyr::mutate(urgency = (frequency + timing + intensity + commitment)/nchar(text)) |>
       dplyr::arrange(-urgency)
   } else if (normalize == "tokens") {
     out <- out |>
-      dplyr::mutate(urgency = (frequency + timing + degree + commit)/nchar(text)) |>
-      dplyr::arrange(-urgency)
-  } else {
-    out <- out |>
-      dplyr::mutate(urgency = (frequency + timing + degree + commit)) |>
+      dplyr::mutate(urgency = (frequency + timing + intensity + commitment)) |>
       dplyr::arrange(-urgency)
   }
   class(out) <- c("urgency", class(out))
@@ -48,98 +44,58 @@ get_urgency <- function(.data, dictionary, normalize) {
   # todo: fix normalization scores, how to best do it?
 }
 
-.assign_frequencies <- function(promises) {
-  frequency_words <-
-  out <- data.frame(sentence = 1:(length(promises[["promises"]])))
-  for (i in names(unlist(unname(freq_adverbs)))) {
-    out[[i]] <- stringr::str_count(promises[["lemmas"]], textstem::lemmatize_strings(i))*
-      unlist(unname(freq_adverbs))[[i]]
+.assign_frequencies <- function(v) {
+  freq_words <- urgency_word_scores[,1:2] %>%
+    tidyr::drop_na() %>%
+    dplyr::mutate(frequency = textstem::lemmatize_words(frequency)) %>%
+    dplyr::group_by(score_frequency) %>%
+    summarise(terms = paste0(frequency, collapse = "|"))
+  out <- list()
+  for (i in seq_len(nrow(freq_words))) {
+    out[[i]] <- stringr::str_count(v, freq_words$terms[i])*
+      freq_words$score_frequency[i]
   }
-  rowSums(out[-1])
+  rowSums(do.call("cbind", out))
 }
 
-.assign_time <- function(promises) {
-  time_adverbs <- list(relative_timepoints = list("last week" = -7/7, "yesterday" = -1/7,
-                                                  "then" = 1/7, "today|now" = 7/7,
-                                                  "tonight" = 3.5/7,
-                                                  "tomorrow" = 4/7, "next week" = 1/7),
-                       relational_timepoints = list("formerly" = -8/8, "previously" = -5/8,
-                                                    "already|since" = -4/8,
-                                                    "lately" = -3/8, "recently" = -2/8,
-                                                    "just|still" = -1/8,
-                                                    "first" = 8/8, "next" = 7/8,
-                                                    "before" = 5.5/8,
-                                                    "early|earlier" = 5.5/8,
-                                                    "soon" = 4/8,
-                                                    "next" = 4/8,
-                                                    "after|afterwards" = 3.5/8,
-                                                    "late" = 3/8, "later" = 2.5/8,
-                                                    "finally" = 2/8, # degree?
-                                                    "eventually" = 1/8,
-                                                    "at some stage" = 0.5/8))
-  out <- data.frame(sentence = 1:(length(promises[["promises"]])))
-  for (i in names(unlist(unname(time_adverbs)))) {
-    out[[i]] <- stringr::str_count(promises[["lemmas"]], textstem::lemmatize_strings(i))*
-      unlist(unname(time_adverbs))[[i]]
+.assign_timing <- function(v) {
+  timing_words <- urgency_word_scores[,3:4] %>%
+    tidyr::drop_na() %>%
+    dplyr::mutate(timing = textstem::lemmatize_words(timing)) %>%
+    dplyr::group_by(score_timing) %>%
+    summarise(terms = paste0(timing, collapse = "|"))
+  out <- list()
+  for (i in seq_len(nrow(timing_words))) {
+    out[[i]] <- stringr::str_count(v, timing_words$terms[i])*
+      timing_words$score_timing[i]
   }
-  rowSums(out[-1])
+  rowSums(do.call("cbind", out))
 }
 
-.assign_degree <- function(promises) {
-  # only for adverbs not in SO-CAL list
-  degr_adverbs <-  list(very_important = c("especially|completely|decidedly|
-                                           |deeply|highly|entirely|practically|
-                                           |really|simply|strongly|totally|utterly|
-                                           |virtually|urgently|obvious|serious|
-                                           |significant|major" = 1),
-                        important = c("lots|very|much|most|fully|far|clearly" = 0.5),
-                        unimportant = c("somewhat|almost|least|less|indeed|quite|rather" = 0.2))
-  out <- data.frame(sentence = 1:(length(promises[["promises"]])))
-  for (i in names(unlist(unname(degr_adverbs)))) {
-    i <- stringr::str_remove_all(i, "|\n")
-    out[[i]] <- stringr::str_count(promises[["lemmas"]], textstem::lemmatize_strings(i))*
-      unlist(unname(degr_adverbs))[[i]]
+.assign_intensity <- function(v) {
+  intensity_words <- urgency_word_scores[,5:6] %>%
+    tidyr::drop_na() %>%
+    dplyr::mutate(intensity = textstem::lemmatize_words(intensity)) %>%
+    dplyr::group_by(score_intensity) %>%
+    summarise(terms = paste0(intensity, collapse = "|"))
+  out <- list()
+  for (i in seq_len(nrow(intensity_words))) {
+    out[[i]] <- stringr::str_count(v, intensity_words$terms[i])*
+      intensity_words$score_intensity[i]
   }
-  rowSums(out[-1])
+  rowSums(do.call("cbind", out))
 }
 
-.assign_commitment <- function(promises) {
-  commit_level <- list(commited = c("will|must|going to|need to|ready to|is time to|
-                                    |commit to|promise to|intend to|urge|stand for|
-                                    |address|take care of|tackle|fix" = 0.2),
-                       not_as_commited = c("should|let's|want to|can|could|may|
-                                           |might|shall|would" = 0.1))
-  out <- data.frame(sentence = 1:(length(promises[["promises"]])))
-  for (i in names(unlist(unname(commit_level)))) {
-    out[[i]] <- stringr::str_count(promises[["lemmas"]], textstem::lemmatize_strings(i))*
-      unlist(unname(commit_level))[[i]]
+.assign_commitment <- function(v) {
+  commitment_words <- urgency_word_scores[,7:8] %>%
+    tidyr::drop_na() %>%
+    dplyr::mutate(commitment = textstem::lemmatize_words(commitment)) %>%
+    dplyr::group_by(score_commitment) %>%
+    summarise(terms = paste0(commitment, collapse = "|"))
+  out <- list()
+  for (i in seq_len(nrow(commitment_words))) {
+    out[[i]] <- stringr::str_count(v, commitment_words$terms[i])*
+      commitment_words$score_commitment[i]
   }
-  rowSums(out[-1])
-}
-
-#' Rank urgent topics
-#'
-#' @param .data A data frame, promises, or text vector.
-#' For data frames, function will search for "text" variable.
-#' For promises data, function will search for "promises" variable.
-#' @param subjects List of subjects.
-#' @import dplyr
-#' @importFrom tidyr separate_rows
-#' @examples
-#' \donttest{
-#' get_urgency_rank(US_News_Conferences_1960_1980[1:10,3])
-#' }
-#' @export
-get_urgency_rank <- function(v, subjects) {
-  topic <- urgency <- urgency_sum <- NULL
-  if (!inherits(v, "urgency")) {
-    v <- get_urgency(v, subjects)
-  }
-  v |> dplyr::mutate(topic = ifelse(topic == "" | is.na(topic), "No topic", topic)) |>
-    tidyr::separate_rows(topic, sep = ", ") |>
-    dplyr::group_by(topic) |>
-    dplyr::summarise(urgency_sum = sum(urgency),
-                     urgency_mean = mean(urgency),
-                     urgency_median = stats::median(urgency)) |>
-    dplyr::arrange(-urgency_sum)
+  rowSums(do.call("cbind", out))
 }
