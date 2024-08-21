1015b5ce984cda6706d8fbf194553688b7c5d7d5
James Hollway <james.hollway@graduateinstitute.ch>
2024-08-21 20:06:53 +0200

    Full stop added at the end to make space for timing and frequency

--- R/sim_urgency.R
+++ R/sim_urgency.R
@@ -15,14 +15,17 @@ sim_urgency <- function(urgency,
     combins$combo <- combins$Rescaled.x * combins$Rescaled.y
     intcom <- combins[which.min(abs(abs(urgency) - combins$combo)),c("Var1","Var2")]
     if(urgency < 0) intcom <- c(intcom, sample(c("not","never"),1))
-    out <- paste(pronoun, paste(intcom, collapse = " "), "do this.")
-  } else if(!missing(commitment)){
-    commit <- comm$word[which.min(abs(abs(commitment) - comm$Rescaled))]
-    if(commitment<0) commit <- paste(commit, sample(c("not","never"),1))
-    if(!missing(intensity)){
-      intensifier <- int$word[which.min(abs(abs(intensity) - int$Rescaled))]
-      out <- paste(pronoun, intensifier, commit, "do this.")
-    } else out <- paste(pronoun, commit, "do this.")
-  } else out <- paste(pronoun, "do this.")
+    out <- paste(pronoun, paste(intcom, collapse = " "), "do this")
+  } else {
+    if(!missing(commitment)){
+      commit <- comm$word[which.min(abs(abs(commitment) - comm$Rescaled))]
+      if(commitment<0) commit <- paste(commit, sample(c("not","never"),1))
+      if(!missing(intensity)){
+        intensifier <- int$word[which.min(abs(abs(intensity) - int$Rescaled))]
+        out <- paste(pronoun, intensifier, commit, "do this")
+      } else out <- paste(pronoun, commit, "do this")
+    } else out <- paste(pronoun, "do this")
+  }
+  out <- paste0(out, ".")
   out
 }
