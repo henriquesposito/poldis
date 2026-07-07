5bd5a48e02bb48c327aff10cad6f5b21992686e7
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2026-07-07 18:37:34 +0200

    Urgency coddebook no longer available due to changes in terms and functions. We will refer to paper in the future.

--- R/utils.R
+++ R/utils.R
@@ -14,16 +14,17 @@ thisRequires <- function(pkgname){
 open_codebook <- function(codebook = "urgency") {
   thisRequires("cli")
   url <- "https://github.com/henriquesposito/poldis/tree/develop/inst"
-  if (codebook == "urgency") {
-    tryCatch({
-      utils::browseURL(paste0(url, "/urgency_codebook.pdf"),
-                       browser = getOption("browser"), encodeIfNeeded = FALSE)
-    }, error = function(e) {
-      message(paste0("Unable to open codebook, please visit: ",
-                     cli::style_hyperlink(paste0(url, "/urgency_codebook.pdf"),
-                                          paste0(url, "/urgency_codebook.pdf"))))
-    })
-  } else if (codebook == "topic") {
+  # if (codebook == "urgency") {
+  #   tryCatch({
+  #     utils::browseURL(paste0(url, "/urgency_codebook.pdf"),
+  #                      browser = getOption("browser"), encodeIfNeeded = FALSE)
+  #   }, error = function(e) {
+  #     message(paste0("Unable to open codebook, please visit: ",
+  #                    cli::style_hyperlink(paste0(url, "/urgency_codebook.pdf"),
+  #                                         paste0(url, "/urgency_codebook.pdf"))))
+  #   })
+  # }
+  if (codebook == "topic") {
     tryCatch({
       utils::browseURL(paste0(url, "/topic.pdf"), browser = getOption("browser"), encodeIfNeeded = FALSE)
     }, error = function(e) {
