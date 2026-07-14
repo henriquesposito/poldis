228117099df1b0412a31236a37f12c6cb764f6db
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2026-07-14 19:35:39 +0200

    Moved raw data to data branch, for now, te reduce size of package

--- R/utils.R
+++ R/utils.R
@@ -13,7 +13,7 @@ thisRequires <- function(pkgname){
 # Helper function to get urgency or topics codebook.
 open_codebook <- function(codebook = "urgency") {
   thisRequires("cli")
-  url <- "https://github.com/henriquesposito/poldis/tree/develop/inst"
+  url <- "https://github.com/henriquesposito/poldis/blob/data/inst"
   # if (codebook == "urgency") {
   #   tryCatch({
   #     utils::browseURL(paste0(url, "/urgency_codebook.pdf"),
